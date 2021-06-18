fae0501582fa9f5d6484ce9da40ea525804ab8af
Tolga Secme <tolgasecme@icloud.com>
2021-06-17 23:21:55 -0600

    post.js and posts.js fixed/ appending the post now

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Post from './Post/Post';
-import { Grid } from '@material-ui/core';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import useStyles from './style';
 
 const Posts = () => {
     const posts = useSelector((state) => state.posts)
     const classes = useStyles();
-    console.log(posts);
     return(
-        <>
+            !posts.length ? <CircularProgress /> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={6} md={6}>
-                <Post />
+                {posts.map((post) =>(
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} />
+                    </Grid>
+                    ))}
             </Grid>
-            </Grid>
-        </>
+            )
     )
 }
 
