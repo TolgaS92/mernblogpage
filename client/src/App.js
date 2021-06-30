b977f16f4cc17179b60acf63cdfd907f648d79b1
Tolga Secme <tolgasecme@icloud.com>
2021-06-30 16:01:27 -0600

    working on profile page

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import { Container} from '@material-ui/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home'
+import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
+import Profile from './components/Profile/Profile';
 
 function App() {
   
@@ -15,7 +16,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route exact path="/auth" component={Auth}/>
-          <Route exact path="/profile" component={Home}/>
+          <Route exact path="/profile" component={Profile}/>
         </Switch>
       </Container>
     </BrowserRouter>
--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Container, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { getPosts } from '../../actions/posts';
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form'
 
 const Profile = () => {
+    const [postId, setPostId]= useState();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+        console.log(postId)
+    }, [postId,dispatch])
+
     return (
-        <div>
-            
-        </div>
+        <Grow in>
+        <Container>
+            {/* {user._id.map(res => ( */}
+          <Grid  container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={5} >
+              <Posts setPostId={setPostId} />
+            </Grid>
+            <Grid item xs={12} sm={3} >
+              <Form postId={postId} setPostId={setPostId}/>
+            </Grid>
+          </Grid>
+          {/* ))} */}
+        </Container>
+      </Grow>
     )
 }
-
 export default Profile;
