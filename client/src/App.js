faa5a1f6765e2b4888d604e127815c1d56c0dd88
Tolga Secme <tolgasecme@icloud.com>
2021-07-06 22:27:08 -0600

    D:/Git/auth route has been locked/ if user logged in it will keep redirecting / / home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Container} from '@material-ui/core';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
@@ -7,8 +7,8 @@ import Auth from './components/Auth/Auth';
 import Profile from './components/Profile/Profile';
 
 function App() {
+  const user = JSON.parse(localStorage.getItem('profile'));
   
-
   return (
     
     <BrowserRouter>
@@ -16,7 +16,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home}/>
-          <Route exact path="/auth" component={Auth}/>
+          <Route exact path="/auth" component={() => (!user ? <Auth /> : <Redirect to="/" />)}></Route>
           <Route exact path="/profile" component={Profile}/>
         </Switch>
       </Container>
