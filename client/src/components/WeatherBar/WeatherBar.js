661eb71bfd6c85bdb09c0b16a56eb02ead47a8ad
Tolga Secme <tolgasecme@icloud.com>
2021-07-08 22:53:09 -0600

    weather api connection is working, needs to figure icons

--- client/src/components/WeatherBar/WeatherBar.js
+++ client/src/components/WeatherBar/WeatherBar.js
@@ -1,31 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardContent, CardMedia, Typography } from '@material-ui/core/';
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
 import useStyles from './style';
 import API from "../../utils/API";
+import moment from 'moment';
 
 const WeatherBar = () => {
     const classes = useStyles();
     const [weather, setWeather]=useState([])
+    const date = moment().format("MMMM Do YYYY");
+
     useEffect(() => {
         API.fetchWeather('denver')
           .then(res => setWeather(res.data.main))
           .catch(err => console.log(err))
       }, [])
+
     return (
         <div>
         <Card className={classes.card}>
-            <CardMedia className={classes.media} image="" title="" />
+            <CardMedia className={classes.media} image="/* {`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} */" title="" />
             <div className={classes.overlay}>
                 <Typography variant="h6">
                     Denver
                 </Typography>
             </div>
             <div className={classes.details}>
-            <Typography variant="body2" color="textSecondary">Temperature: {weather.temp}°</Typography>
+            <Typography variant="body2" color="textSecondary">Temperature: {weather.temp}˚F</Typography>
             </div>
-            <Typography className={classes.title} variant="h5">Day</Typography>
+            <Typography className={classes.title} variant="h5">{date}</Typography>
             <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p" gutterBottom>Explanation</Typography>
+                <Typography variant="body2" color="textSecondary" component="p" gutterBottom>Humidity: {weather.humidity}%</Typography>
             </CardContent>
         </Card>
         </div>
